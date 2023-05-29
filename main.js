867aae5ca94e0c987c6a87d7c4f89d9d2307a205
shalinispk7 <shalinispk7@gmail.com>
2023-05-29 20:23:07 +0530

    person age is eligible for casting vote

---
+++ main.js
@@ -0,0 +1,7 @@
+let age=parseInt(prompt("Enter your age"));
+if(age>=18){
+document.write("The age of person is "+age+" .The person is eligible for casting vote");
+}
+else{
+document.write("The age of person is "+age+" .The person is   not eligible for casting vote");
+}
